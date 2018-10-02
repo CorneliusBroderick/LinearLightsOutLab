e0fb0a7f1e1ffe28a69c5bac707bc4deefe0d1b0
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-10-02 12:15:28 +0100

    Starting main work

--- LinearLightsOutLab/app/src/main/java/ie/ul/cbroderick/linearlightsoutlab/MainActivity.java
+++ LinearLightsOutLab/app/src/main/java/ie/ul/cbroderick/linearlightsoutlab/MainActivity.java
@@ -2,12 +2,39 @@ package ie.ul.cbroderick.linearlightsoutlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LightsOutGame mGame;
+    private TextView mGameStateTextView;
+    private Button[] mButtons;
+
+    private static final int NUM_SQUARES = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGame = new LightsOutGame(NUM_SQUARES);
+
+        mGameStateTextView = findViewById(R.id.game_state_text_view);
+        mButtons = new Button[NUM_SQUARES];
+        mButtons[0] = findViewById(R.id.button0);
+        mButtons[1] = findViewById(R.id.button1);
+        mButtons[2] = findViewById(R.id.button2);
+        mButtons[3] = findViewById(R.id.button3);
+        mButtons[4] = findViewById(R.id.button4);
+        mButtons[5] = findViewById(R.id.button5);
+        mButtons[6] = findViewById(R.id.button6);
+        updateView();
+
+    }
+
+    private void updateView() {
+        //mGameStateTextView.setText(mGame.getNumPresses());
+
     }
 }
